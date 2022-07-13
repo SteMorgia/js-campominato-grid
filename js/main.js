2089615c3f3a3180424cdd4c82ffda4dabeb9414
Stefano Morgia <ste.morgia@gmail.com>
2022-07-13 17:05:03 +0200

    esercizio base completato, bonus in corso

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 const gridDom = document.getElementById('grid');
+let myArray = [];
 for (let i = 0; i < 100; i++) {
     const divSquare = document.createElement('div');
     gridDom.append(divSquare);
     divSquare.classList.add('square');
+    console.log(i + 1);
+    divSquare.append(i + 1);
 }
 
 
 
 
 
-function generaNumeri ()
+function generaNumeri (min, max) {
+    return (Math.round(Math.random(max - min) * 100));
+}
+/*funxtion creaQuadrati(sqaure_number) {
+    for(i < sqaurenumber)
+}
+
+
+creaQuadrati(100) */
